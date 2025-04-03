4c41c8ba482acf9b366e28b747569ad029a6b09b
Philippa Broadbent <phillybroadbent@gmail.com>
2025-04-03 10:08:49 +0100

    Added comments to data analysis

--- eva_data_analysis.py
+++ eva_data_analysis.py
@@ -9,6 +9,10 @@ input_file = open('./eva-data.json', 'r', encoding="utf-8")
 output_file = open('./eva-data.csv','w', encoding="utf-8")
 graph_file = './cumulative_eva_graph.png'  
 
+
+print("--START--")
+print(f'Reading JSON file {input_file}')
+
 # read in json file
 eva_df = pd.read_json(input_file, convert_dates=['date'])
 # convert eva column to float
@@ -25,9 +29,12 @@ eva_df.to_csv(output_file, index=False)
 eva_df['duration_hours'] = eva_df['duration'].str.split(":").apply(lambda x: int(x[0])+ int(x[1])/60)
 eva_df['cumulative_time'] = eva_df['duration_hours'].cumsum()
 
+print(f'Plotting cumulative spacewalk duration and saving to {graph_file}')
+# Plot cumulative time spent in space over years
 plt.plot(eva_df['date'], eva_df['cumulative_time'], 'ko-')
 plt.xlabel('Year')
 plt.ylabel('Total time spent in space to date (hours)')
 plt.tight_layout()
 plt.savefig(graph_file)
 plt.show()
+print("--END--")
