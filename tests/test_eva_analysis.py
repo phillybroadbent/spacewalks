c06c9c5070a9ec6f5d42ee513824bdd49a67d9c9
phillybroadbent <phillybroadbent@gmail.com>
2025-11-27 12:38:31 +0000

    Add test suite

---
+++ tests/test_eva_analysis.py
@@ -0,0 +1,31 @@
+import pytest
+from eva_data_analysis import text_to_duration, calculate_crew_size
+
+def test_text_to_duration_integer():
+    """
+    Test that text_to_duration returns expected ground truth values
+    for typical whole hour durations 
+    """
+    assert text_to_duration("10:00") == 10
+    
+def test_text_to_duration_float():
+    """
+    Test that text_to_duration returns expected ground truth values
+    for typical durations with a non-zero minute component
+    """
+    assert text_to_duration("10:20") == pytest.approx(10.33333333)
+    
+    
+@pytest.mark.parametrize("input_value, expected_result", [
+    ("Valentina Tereshkova;", 1),
+    ("Judith Resnik; Sally Ride;", 2),
+])
+def test_calculate_crew_size(input_value, expected_result):
+    """
+    Test that calculate_crew_size returns expected ground truth values
+    for typical crew values
+    """
+    actual_result = calculate_crew_size(input_value)
+    assert actual_result == expected_result
+    
+
